4992781cd4a4e501b4ca946aa2c1d228e8d29c2c
Felipe Castro <andresfelipecao@gmail.com>
2023-10-04 21:25:01 -0500

    m

--- src/component/SkillsCard.jsx
+++ src/component/SkillsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SkillsCard = ({name,url}) => {
   return (
     <>
-      <div className="col text-center">
+      <div className="col-md-2 col-4 text-center">
           <img src={url} alt={name}  width='50px' height='50px' style={{filter:'grayscale(1)'}} />
           <h5 className='text-center mt-1'>{name}</h5>
       </div>
